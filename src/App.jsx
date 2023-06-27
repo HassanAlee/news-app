1469ce3227dce5783d5e0640a20ef080686b3680
Hassan Ali <hassanali14563@gmail.com>
2023-06-27 11:48:08 +0500

    Added search form

--- src/App.jsx
+++ src/App.jsx
@@ -38,6 +38,20 @@ const App = ({ load, news }) => {
   return (
     <>
       <section id="news" className="my-4">
+        <form className="flex justify-center mb-6">
+          <input
+            type="text"
+            className="shadow appearance-none border rounded w-1/2 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="inputField"
+          />
+          <br />
+          <button
+            type="submit"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          >
+            Search
+          </button>
+        </form>
         <h1 className="text-3xl font-bold underline text-center">
           Latest News
         </h1>
